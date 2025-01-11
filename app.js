3b95eb3236a045712d81b4e39c12c88f4aedfaab
nimithanizar <nimithanizar2000@gmail.com>
2025-01-11 17:38:55 +0530

    feat: product schema and routes created

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const productRoutes = require('./routes/ProductRoutes');
 
 dotenv.config();
 
@@ -9,6 +10,9 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+
+
+app.use('/api/products', productRoutes);
 
 app.get('/', (req, res) => {
     res.send('API is running...');
---
+++ routes/ProductRoutes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const Product = require('../models/Products ');
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+  try {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.status(201).json(newProduct);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+module.exports = router;
